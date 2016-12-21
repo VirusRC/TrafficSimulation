f5acc5be93958fd2ac038ce8ef151a79fa1bd77c
VirusRC <thomas.mike@gmx.at>
2016-12-21 16:42:34 +0100

    First commit

---
+++ PiApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
