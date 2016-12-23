75282b42acdb024e25efa40c4a339a1cb5139d0d
Mike Thomas <fhs36415@fh-salzburg.ac.at>
2016-12-23 15:10:33 +0100

    Prototype implementation of named pipes (same network)

--- Prototype/PrototypeServer.cs
+++ Prototype/PrototypeServer.cs
@@ -11,45 +11,32 @@ namespace Prototype
 {
     class PrototypeServer
     {
-		private static int numThreads = 4;
-
-		public static void Main()
-		{
-			int i;
-			Thread[] servers = new Thread[numThreads];
-
-			Console.WriteLine("\n*** Named pipe server stream with impersonation example ***\n");
-			Console.WriteLine("Waiting for client connect...\n");
-			for(i = 0; i < numThreads; i++)
-			{
-				servers[i] = new Thread(serverThread);
-				servers[i].Start();
-			}
-			Thread.Sleep(250);
-			while(i > 0)
-			{
-				for(int j = 0; j < numThreads; j++)
-				{
-					if(servers[j] != null)
-					{
-						if(servers[j].Join(250))
-						{
-							Console.WriteLine("Server thread[{0}] finished.", servers[j].ManagedThreadId);
-							servers[j] = null;
-							i--;    // decrement the thread watch count
-						}
-					}
-				}
-			}
-			Console.WriteLine("\nServer threads exhausted, exiting.");
-		}
+        public static void Main()
+        {
+            Thread server = new Thread(serverThread);
+            server.Start();
+            Console.WriteLine("\nServer started.");
+            int i = 1;
+            while (i > 0)
+            {
+                if (server != null)
+                {
+                    if (server.Join(250))
+                    {
+                        server = null;
+                        i--;    // decrement the thread watch count
+                    }
+                }
+            }
+            Console.WriteLine("\nServer threads exhausted, exiting.");
+            Console.ReadLine();
+        }
 
 
-		private static void serverThread()
+        private static void serverThread()
         {
             // Creating a new pipeServer object.
-            NamedPipeServerStream pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, 1);
-            int threadId = Thread.CurrentThread.ManagedThreadId;
+            NamedPipeServerStream pipeServer = new NamedPipeServerStream("IncommingPipe", PipeDirection.In, 1);
 
             //Waiting for a client to connect
             pipeServer.WaitForConnection();
@@ -58,19 +45,17 @@ namespace Prototype
                 // Read the request from the client. Once the client has written to the pipe its security token will be available.
                 StreamString ss = new StreamString(pipeServer);
 
-                // Verify our identity to the connected client using a string that the client anticipates.
-                ss.WriteString("I am the one true server!");
-                string filename = ss.ReadString();
-
-                // Read in the contents of the file while impersonating the client.
-                ReadFileToStream fileReader = new ReadFileToStream(ss, filename);
-
-                // Display the name of the user we are impersonating.
-                Console.WriteLine("Reading file: {0} on thread[{1}] as user: {2}.",
-                    filename, threadId, pipeServer.GetImpersonationUserName());
-                pipeServer.RunAsClient(fileReader.Start);
+                while (true)
+                {
+                    string clientstring = ss.ReadString();
+                    if (clientstring != string.Empty && clientstring != null)
+                    {
+                        Console.WriteLine(clientstring);
+                    }
+                    Thread.Sleep(250);
+                }
             }
-            catch (IOException ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("ERROR: {0}", ex.Message);
             }
