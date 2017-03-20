6afb5d015e0329fbd85602f2a9b888bf6e54b1ad
VirusRC <thomas.mike@gmx.at>
2017-03-20 20:08:32 +0100

    Adding new version of DroneCommands_Executor

--- PythonDroneControl/DroneCommands_Executor.py
+++ PythonDroneControl/DroneCommands_Executor.py
@@ -16,8 +16,9 @@ from bson import ObjectId
 #Drone commands for takeoff and 
 droneTakeoff = rospy.Publisher("/bebop/takeoff",Empty)
 droneLanding = rospy.Publisher("/bebop/land",Empty)
+droneControl = rospy.Publisher("/bebop/cmd_vel",Twist)
 
-client = MongoClient('193.171.53.69', 27017)
+client = MongoClient('193.171.53.68', 27017)
 db = client['DroneDB']
 collection = db['DroneCommands']
 
@@ -38,13 +39,22 @@ if __name__ == '__main__':
    					if command == "land":
         					print("Drone is landing...") 
 						droneLanding.publish(Empty())
+					if command == "control":
+						print("Drone is moving...")
+						k = currentDocument["Velocity_y"]
+						#if float(k) < 0:
+						#	droneControl.publish(Twist(Vector3(0,0,-0.1),Vector3(0,0,0)))
+						#if float(k) > 0:
+						#	droneControl.publish(Twist(Vector3(0,0,0.1),Vector3(0,0,0)))
+						velocity_y = 0.5 * float(k)
+						droneControl.publish(Twist(Vector3(0,0,velocity_y),Vector3(0,0,0)))
 				except rospy.ROSInterruptException:
         				pass
 				_id=currentDocument["_id"]
 				collection.delete_one({'_id': ObjectId(_id)})
 			else:
 				print("No command available!")
-			time.sleep(2)
+			#time.sleep(1)
 	except KeyboardInterrupt:
 		print("Closing programm!")
 
