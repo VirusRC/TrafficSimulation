198267107b18f1f499bb735d198a937436828440
Fabian Schörghofer <fabian.schoergi@gmail.com>
2017-04-06 14:39:10 +0200

    travis

---
+++ scripts/script.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# This script is intendet to prepare the project for submission, put it into
+#  the final zip-Container and deploy it to a certain server.
+
+REPO=https://github.com/elektm93/traffic_sim.git
+SUBZIP=/tmp/Submission.zip
+TMPGITPATH=/tmp/traffic_sim
+
+### create doc
+
+# doxygen
+# doxygen src/doxygenConfig
+
+# latex # TODO: twice??
+cd doc; pdflatex Documentation.tex
+cd doc; pdflatex Documentation.tex
+cd doc; pdflatex Documentation.tex
+
+### put everything together into an zip container
+git clone $REPO $TMPGITPATH
+
+# doc
+cd doc; zip $SUBZIP -r Documentation.pdf
+
+# src
+cd /tmp/traffic_sim; zip $SUBZIP -r src/
+
+# vid
+## TODO
+
+
+
+### "deploy"
