172ee57bb2b440eddeb138b8fe614de27c950257
Mike Thomas <fhs36415@fh-salzburg.ac.at>
2017-04-29 11:16:08 +0200

    Added simple IPC testlogic

--- src/TrafficLights_Control/Client/IpcClient.cs
+++ src/TrafficLights_Control/Client/IpcClient.cs
@@ -1,14 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 
 namespace Client
 {
-    public class IpcClient
+  public class IpcClient
+  {
+    #region ### PRIVATE PROPERTIES ###
+
+    #endregion
+
+    #region ### PUBLIC PROPERTIES ###
+    public RemoteObject.RemoteObject MyRemoteObject;
+    #endregion
+
+
+    #region ### PRIVATE METHODS ###
+
+    #endregion
+
+    #region ### PUBLIC METHODS ###
+
+    public bool StartRemoteConnection(string hostname)
     {
-      
-      
+      if (String.IsNullOrEmpty(hostname))
+      {
+        return false;
+      }
+      try
+      {
+        // Create the channel.
+        TcpChannel clientChannel = new TcpChannel();
+
+        // Register the channel.
+        ChannelServices.RegisterChannel(clientChannel, false);
+
+        // Register as client for remote object.
+        WellKnownClientTypeEntry remoteType = new WellKnownClientTypeEntry(
+          typeof(RemoteObject.RemoteObject), $"tcp://{hostname}:{RemoteObject.RemoteObject.GetIpcPort()}/{RemoteObject.RemoteObject.GetRemoteObjectIdentifier()}");
+        RemotingConfiguration.RegisterWellKnownClientType(remoteType);
+
+        // Create a message sink.
+        //string objectUri;
+        //System.Runtime.Remoting.Messaging.IMessageSink messageSink =
+        //  clientChannel.CreateMessageSink(
+        //    $"tcp://{hostname}:{RemoteObject.RemoteObject.GetIpcPort()}/{RemoteObject.RemoteObject.GetRemoteObjectIdentifier()}", null,
+        //    out objectUri);
+
+        // Create an instance of the remote object.
+        MyRemoteObject = new RemoteObject.RemoteObject();
+
+        // Invoke a method on the remote object.
+        MyRemoteObject.GetCount();
+
+        return true;
+      }
+      catch (Exception ex)
+      {
+        Debug.Print(ex.Message);
+        return false;
+      }
     }
+    #endregion
+  }
 }
--- src/TrafficLights_Control/Client_TestApp/Client_TestApp.cs
+++ src/TrafficLights_Control/Client_TestApp/Client_TestApp.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Client;
 
 namespace Client_TestApp
 {
   class ClientTestApp
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+      IpcClient tmp = new IpcClient();
 
+      tmp.StartRemoteConnection("localhost");
 
 
 
