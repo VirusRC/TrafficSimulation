06f2b0b4dfe5de0d81aeda278c718a1aceb85a4c
Mike Thomas <fhs36415@fh-salzburg.ac.at>
2017-05-03 13:46:28 +0200

    Added bug fix delay at server request
    Prototype implementation has to be done properly.

--- Unity/TrafficSimulation/Assets/Scripts/Simulation.cs
+++ Unity/TrafficSimulation/Assets/Scripts/Simulation.cs
@@ -1,53 +1,69 @@
-﻿using Client;
+﻿
+using System.Linq;
+using Assets.Scripts;
+using Client;
 using RemoteObject;
 
-public class Simulation{
+public class Simulation
+{
 
-    private static Simulation instance;
-    private IpcClient ipcClient;
+  private static Simulation instance;
+  private IpcClient ipcClient;
 
-    //private string ipAddressServer= "78.104.168.206";
-    private string ipAddressServer = "localhost";
+  //private string ipAddressServer= "78.104.168.206";
+  private string ipAddressServer = "pwnhofer.at";
 
-    private Simulation()
+  private Simulation()
+  {
+    instance = this;
+    ipcClient = new IpcClient();
+    ipcClient.StartRemoteConnection(ipAddressServer);
+  }
+
+  public static Simulation getInstance()
+  {
+    if (instance == null)
     {
-        instance = this;
-        ipcClient = new IpcClient();
-        ipcClient.StartRemoteConnection(ipAddressServer);
+      new Simulation();
     }
+    return instance;
+  }
 
-    public static Simulation getInstance()
-    {
-        if (instance == null)
-        {
-            new Simulation();
-        }
-        return instance;
-    }
+  public RemoteObject.RemoteObject getRemoteObject()
+  {
+    return null;
+  }
 
-    public RemoteObject.RemoteObject getRemoteObject()
-    {
-        return null;
-    }
+  public void createNewTrafficLight(string uuid, string id1, string id2, string id3, string id4)
+  {
+    ipcClient.CreateIntersection(uuid, id1, id2, id3, id4);
+    TrafficLightsBuffer test = TrafficLightsBuffer.Instance;
+    test.lstTLs.Add(new myTL(uuid, id1));
+    test.lstTLs.Add(new myTL(uuid, id2));
+    test.lstTLs.Add(new myTL(uuid, id3));
+    test.lstTLs.Add(new myTL(uuid, id4));
+  }
 
-    public void createNewTrafficLight(string uuid, string id1, string id2, string id3, string id4)
-    {
-        ipcClient.CreateIntersection(uuid,id1,id2,id3,id4);
-    }
+  public void createNewTrafficLight(string uuid, string id1, string id2, string id3)
+  {
+    ipcClient.CreateIntersection(uuid, id1, id2, id3);
+    TrafficLightsBuffer test = TrafficLightsBuffer.Instance;
+    test.lstTLs.Add(new myTL(uuid, id1));
+    test.lstTLs.Add(new myTL(uuid, id2));
+    test.lstTLs.Add(new myTL(uuid, id3));
+  }
 
-    public void createNewTrafficLight(string uuid, string id1, string id2, string id3)
-    {
-        ipcClient.CreateIntersection(uuid, id1, id2, id3);
-    }
+  public Enum.TrafficLightsStatus getTrafficLightState(string uuid, string id)
+  {
+    //return ipcClient.GetTrafficLightsStatus(uuid, id);
+    TrafficLightsBuffer tmp = TrafficLightsBuffer.Instance;
+    var result = tmp.lstTLs.Where(item => item.uuid == uuid && item.id == id).First().status;
+    return result;
+  }
 
-    public Enum.TrafficLightsStatus getTrafficLightState(string uuid, string id)
-    {
-        return ipcClient.GetTrafficLightsStatus(uuid,id);
-    }
-
-    public float getCarSpeed()
-    {
-        return 10f;
-    }
+  public float getCarSpeed()
+  {
+    return 10f;
+  }
 
 }
---
+++ Unity/TrafficSimulation/Assets/Scripts/TrafficLightsBuffer.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading;
+using Boo.Lang;
+using RemoteObject;
+
+
+namespace Assets.Scripts
+{
+  /// <summary>
+  /// Singleton class which holds the current information of the traffic lights.
+  /// </summary>
+  public class TrafficLightsBuffer
+  {
+    private static TrafficLightsBuffer instance;
+
+    public List<myTL> lstTLs = new List<myTL>();
+
+    private TrafficLightsBuffer() { }
+
+    public static TrafficLightsBuffer Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new TrafficLightsBuffer();
+          //start thread when object is created
+          Thread thread = new Thread(updateTLstatus);
+          thread.Start();
+        }
+        return instance;
+      }
+    }
+
+    public static void updateTLstatus()
+    {
+      var lala = Simulation.getInstance();
+      TrafficLightsBuffer tmp = instance;
+      while (true)
+      {
+        foreach (var item in tmp.lstTLs)
+        {
+          item.status = lala.getTrafficLightState(item.uuid, item.id);
+        }
+        Thread.Sleep(200);
+      }
+    }
+  }
+
+  public class myTL
+  {
+    public myTL(string uuid, string id)
+    {
+      this.uuid = uuid;
+      this.id = id;
+    }
+
+    public string uuid;
+    public string id;
+
+    public Enum.TrafficLightsStatus status;
+  }
+
+}
