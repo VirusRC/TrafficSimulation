3c615764c2b4717eb291b18edd5ca2b2588142dd
Andreas Reschenhofer <andreas-reschenhofer@gmx.at>
2017-05-05 20:43:12 +0200

    More streets. Car acceleration.

--- Unity/TrafficSimulation/Assets/Scripts/FollowWay.cs
+++ Unity/TrafficSimulation/Assets/Scripts/FollowWay.cs
@@ -4,55 +4,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FollowWay : MonoBehaviour {
+public class FollowWay : MonoBehaviour
+{
 
 	public float maxSpeed;
 	public Slider maxSpeedSlider;
 
 	private float speed = 0f;
-    private float rotationSpeed = 2f;
+	private float rotationSpeed = 2f;
 	private float mass = 2000f;
 	private float gravity = 9.81f;
 	private float friction = 0.8f;
 	private float ps = 200f;
 
-    private GameObject pathGO;
-    Transform targetPathNode;
-    int pathNodeIndex = 0;
+	private GameObject pathGO;
+	Transform targetPathNode;
+	int pathNodeIndex = 0;
 
-    private GameObject crossingNext;
-    private CrossingColliderT nextCrossingColliderT;
-    private CrossingColliderX nextCrossingColliderX;
-    private bool nextCrossingIsControlled;
+	private GameObject crossingNext;
+	private CrossingColliderT nextCrossingColliderT;
+	private CrossingColliderX nextCrossingColliderX;
+	private bool nextCrossingIsControlled;
 
-    private GameObject actualStreet;
+	private GameObject actualStreet;
 
-    private string direction;
-    private string generalDirection;
-    private bool isNewStreet;
-    private int streetIndex;
+	private string direction;
+	private string generalDirection;
+	private bool isNewStreet;
+	private int streetIndex;
 
-    int countTraffic = 0;
+	int countTraffic = 0;
 
-    //private Rigidbody rigedBody;
+	//private Rigidbody rigedBody;
 
-    // Use this for initialization
-    void Start () {
-        speed = Simulation.getInstance().getCarSpeed();
+	// Use this for initialization
+	void Start()
+	{
+		speed = Simulation.getInstance().getCarSpeed();
 		maxSpeedSlider = GameObject.Find("SliderMaxCarSpeed").GetComponent<Slider>();
 		maxSpeed = maxSpeedSlider.value;
 		maxSpeedSlider.onValueChanged.AddListener(delegate { maxSpeedValueChangeCheck(); });
-		
+
 		//rigedBody = GetComponent<Rigidbody>();
 	}
 
 
-    public void initialize(GameObject street, string direction)
-    {
-        this.direction = direction;
-        this.actualStreet = street;
-        isNewStreet = true;
-    }
+	public void initialize(GameObject street, string direction)
+	{
+		this.direction = direction;
+		this.actualStreet = street;
+		isNewStreet = true;
+	}
 
 	private void accelerate()
 	{
@@ -60,22 +62,22 @@ public class FollowWay : MonoBehaviour {
 		speed = speed + a;
 	}
 
-	private void brake()
+	private void brake(float distance)
 	{
-		float a = ps / mass;
-		speed = speed - a * 2;
+		speed = speed - speed / distance;
 	}
 
-    // Update is called once per frame
-    void Update () {
-        if (targetPathNode == null)
-        {
-            GetNextPathNode();
-        }
-        Vector3 dir = targetPathNode.position;
-        dir.y = 1.3f;
-        dir = dir - this.transform.localPosition;
-		
+	// Update is called once per frame
+	void Update()
+	{
+		if(targetPathNode == null)
+		{
+			GetNextPathNode();
+		}
+		Vector3 dir = targetPathNode.position;
+		dir.y = 1.3f;
+		dir = dir - this.transform.localPosition;
+
 		if(speed < maxSpeed) //&& no collision detected
 		{
 			//speed += 0.1f;
@@ -84,25 +86,25 @@ public class FollowWay : MonoBehaviour {
 		if(speed > maxSpeed)
 		{
 			//speed -= 0.2f;
-			brake();
+			brake(20f);
 		}
 		rotationSpeed = speed / 2f;
-		resizeCollider();
+		//resizeCollider();
 
 		float distThisFrame = speed * Time.deltaTime;
 
-        if (dir.magnitude <= distThisFrame)
-        {
-            targetPathNode = null;
-        }
-        else
-        {
-            //rigedBody.MovePosition(dir.normalized * distThisFrame);
-            transform.Translate(dir.normalized * distThisFrame, Space.World);
-            Quaternion targetRotation = Quaternion.LookRotation(dir);
-            //rotationSpeed = dir.magnitude * speed / 10f;
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime*rotationSpeed);
-        }
+		if(dir.magnitude <= distThisFrame)
+		{
+			targetPathNode = null;
+		}
+		else
+		{
+			//rigedBody.MovePosition(dir.normalized * distThisFrame);
+			transform.Translate(dir.normalized * distThisFrame, Space.World);
+			Quaternion targetRotation = Quaternion.LookRotation(dir);
+			//rotationSpeed = dir.magnitude * speed / 10f;
+			this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+		}
 	}
 
 	void OnCollisionEnter(Collision collisionInfo)
@@ -110,7 +112,33 @@ public class FollowWay : MonoBehaviour {
 		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
 		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
 		print("Their relative velocity is " + collisionInfo.relativeVelocity);
-		brake();
+		var colliededObjectPosition = collisionInfo.collider.gameObject.transform.position;
+		var ourPosition = gameObject.transform.position;
+		float distance = 0;
+		if((ourPosition.x + colliededObjectPosition.x) > (ourPosition.z + colliededObjectPosition.z))
+		{
+			if(colliededObjectPosition.x > ourPosition.x)
+			{
+				distance = colliededObjectPosition.x - ourPosition.x;
+			}
+			if(colliededObjectPosition.x < ourPosition.x)
+			{
+				distance = ourPosition.x - colliededObjectPosition.x;
+			}
+		}
+		else
+		{
+			if(colliededObjectPosition.z > ourPosition.z)
+			{
+				distance = colliededObjectPosition.z - ourPosition.z;
+			}
+			if(colliededObjectPosition.z < ourPosition.z)
+			{
+				distance = ourPosition.z - colliededObjectPosition.z;
+			}
+		}
+
+		brake(distance);
 	}
 
 	private void resizeCollider()
@@ -119,7 +147,7 @@ public class FollowWay : MonoBehaviour {
 		float radius = speed * 0.01f;
 		float height = speed * 0.04f;
 		float centerZ = height / 2f - radius;
-		
+
 		Vector3 center = new Vector3(0f, 0f, centerZ);
 		collider.center = center;
 		collider.height = height;
@@ -127,130 +155,130 @@ public class FollowWay : MonoBehaviour {
 	}
 
 	void GetNextPathNode()
-    {
-        if(pathGO==null || pathNodeIndex >= pathGO.transform.childCount)
-        {
-            getNextStreetPart();
-        }
-        targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
-        pathNodeIndex++;
-    }
+	{
+		if(pathGO == null || pathNodeIndex >= pathGO.transform.childCount)
+		{
+			getNextStreetPart();
+		}
+		targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
+		pathNodeIndex++;
+	}
 
-    private void getNextStreetPart()
-    {
-        if (crossingNext != null)
-        {
-            pathGO = crossingNext;
-            crossingNext = null;
-            pathNodeIndex = 0;
-            targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
-            isNewStreet = true;
-        }
-        else if (actualStreet != null)
-        {
-            if (isNewStreet)
-            {
-                generalDirection = direction;
-                if (generalDirection.Equals("PathPos"))
-                {
-                    streetIndex = 0;
-                }
-                else if (generalDirection.Equals("PathNeg"))
-                {
-                    streetIndex = actualStreet.transform.childCount - 1;
-                }
-                isNewStreet = false;
-            }
+	private void getNextStreetPart()
+	{
+		if(crossingNext != null)
+		{
+			pathGO = crossingNext;
+			crossingNext = null;
+			pathNodeIndex = 0;
+			targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
+			isNewStreet = true;
+		}
+		else if(actualStreet != null)
+		{
+			if(isNewStreet)
+			{
+				generalDirection = direction;
+				if(generalDirection.Equals("PathPos"))
+				{
+					streetIndex = 0;
+				}
+				else if(generalDirection.Equals("PathNeg"))
+				{
+					streetIndex = actualStreet.transform.childCount - 1;
+				}
+				isNewStreet = false;
+			}
 
-            if (generalDirection.Equals("PathPos"))
-            {
-                if (streetIndex > actualStreet.transform.childCount)
-                {
-                    System.Console.WriteLine("destroyed");
-                    Destroy(gameObject);
-                }
-                pathGO = getChildGameObject(actualStreet.transform.GetChild(streetIndex).gameObject, direction);
-                streetIndex++;
-                pathNodeIndex = 0;
-            }
-            else if (generalDirection.Equals("PathNeg"))
-            {
-                if (streetIndex < 0)
-                {
-                    System.Console.WriteLine("destroyed");
-                    Destroy(gameObject);
-                }
-                pathGO = getChildGameObject(actualStreet.transform.GetChild(streetIndex).gameObject, direction);
-                streetIndex--;
-                pathNodeIndex = 0;
-            }
-            else
-            {
-                System.Console.WriteLine("destroyed");
-                Destroy(gameObject);
-            }
-        }
-        else
-        {
-            System.Console.WriteLine("destroyed");
-            Destroy(gameObject);
-        }
-    }
+			if(generalDirection.Equals("PathPos"))
+			{
+				if(streetIndex > actualStreet.transform.childCount)
+				{
+					System.Console.WriteLine("destroyed");
+					Destroy(gameObject);
+				}
+				pathGO = getChildGameObject(actualStreet.transform.GetChild(streetIndex).gameObject, direction);
+				streetIndex++;
+				pathNodeIndex = 0;
+			}
+			else if(generalDirection.Equals("PathNeg"))
+			{
+				if(streetIndex < 0)
+				{
+					System.Console.WriteLine("destroyed");
+					Destroy(gameObject);
+				}
+				pathGO = getChildGameObject(actualStreet.transform.GetChild(streetIndex).gameObject, direction);
+				streetIndex--;
+				pathNodeIndex = 0;
+			}
+			else
+			{
+				System.Console.WriteLine("destroyed");
+				Destroy(gameObject);
+			}
+		}
+		else
+		{
+			System.Console.WriteLine("destroyed");
+			Destroy(gameObject);
+		}
+	}
 
-    static private GameObject getChildGameObject(GameObject fromGameObject, string withName)
-    {
-        Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
-        foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
-        return null;
-    }
+	static private GameObject getChildGameObject(GameObject fromGameObject, string withName)
+	{
+		Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
+		foreach(Transform t in ts) if(t.gameObject.name == withName) return t.gameObject;
+		return null;
+	}
 
-    public void decideWay(CrossingColliderX collider)
-    {
-        if (!isNewStreet)
-        {
-            System.Random random = new System.Random();
-            int randomNumber = random.Next(0, 3);
-            this.nextCrossingColliderT = null;
-            this.nextCrossingColliderX = collider;
-            collider.setDirection(randomNumber, this);
-        }
+	public void decideWay(CrossingColliderX collider)
+	{
+		if(!isNewStreet)
+		{
+			System.Random random = new System.Random();
+			int randomNumber = random.Next(0, 3);
+			this.nextCrossingColliderT = null;
+			this.nextCrossingColliderX = collider;
+			collider.setDirection(randomNumber, this);
+		}
 
-    }
+	}
 
-    public void decideWay(CrossingColliderT collider)
-    {
-        if (!isNewStreet)
-        {
-            System.Random random = new System.Random();
-            int randomNumber = random.Next(0, 2);
-            this.nextCrossingColliderT = collider;
-            this.nextCrossingColliderX = null;
-            collider.setDirection(randomNumber, this);
-        }
-    }
+	public void decideWay(CrossingColliderT collider)
+	{
+		if(!isNewStreet)
+		{
+			System.Random random = new System.Random();
+			int randomNumber = random.Next(0, 2);
+			this.nextCrossingColliderT = collider;
+			this.nextCrossingColliderX = null;
+			collider.setDirection(randomNumber, this);
+		}
+	}
 
-    public void setNewDirection(GameObject crossingWay, GameObject nextWay, bool isControlled)
-    {
-        this.crossingNext = crossingWay;
-        this.actualStreet = nextWay;
-    }
+	public void setNewDirection(GameObject crossingWay, GameObject nextWay, bool isControlled)
+	{
+		this.crossingNext = crossingWay;
+		this.actualStreet = nextWay;
+	}
 
-    public void setNewDirection(GameObject crossingWay, GameObject nextWay, bool isControlled, string uuidTrafficLight, string nameTrafficLight)
-    {
-        this.crossingNext = crossingWay;
-        this.actualStreet = nextWay;
-        this.nextCrossingIsControlled=isControlled;
-    }
+	public void setNewDirection(GameObject crossingWay, GameObject nextWay, bool isControlled, string uuidTrafficLight, string nameTrafficLight)
+	{
+		this.crossingNext = crossingWay;
+		this.actualStreet = nextWay;
+		this.nextCrossingIsControlled = isControlled;
+	}
 
-    public void setStreetDirection(string dir)
-    {
-        this.direction = dir;  
-    }
+	public void setStreetDirection(string dir)
+	{
+		this.direction = dir;
+	}
 
 	public void maxSpeedValueChangeCheck()
 	{
 		maxSpeed = maxSpeedSlider.value;
 	}
 
-	
+
 }
--- Unity/TrafficSimulation/Assets/Scripts/Spawn.cs
+++ Unity/TrafficSimulation/Assets/Scripts/Spawn.cs
@@ -4,72 +4,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Spawn : MonoBehaviour {
+public class Spawn : MonoBehaviour
+{
 
-    public int maxCars;
-    public GameObject startPoint;
-    public string startDirection;
+	public int maxCars;
+	public GameObject startPoint;
+	public string startDirection;
 	public Slider maxCarSlider;
 	public float spawnDuration;
 	public Slider spawnDurationSlider;
 
 	private int currentCars;
-    private int time;
+	private int time;
 
 	// Use this for initialization
-	void Start () {
-        currentCars = 0;
-        time = 0;
+	void Start()
+	{
+		currentCars = 0;
+		time = 0;
 		//Adds a listener to the main slider and invokes a method when the value changes.
 		maxCarSlider.onValueChanged.AddListener(delegate { maxCarsValueChangeCheck(); });
 		spawnDurationSlider = GameObject.Find("SliderCarSpawnDuration").GetComponent<Slider>();
 		spawnDuration = spawnDurationSlider.value;
 		spawnDurationSlider.onValueChanged.AddListener(delegate { spawnDurationValueChangeCheck(); });
 		generateCar();
-    }
+	}
 
-    private void FixedUpdate()
-    {
-        time++;
-        if (time >= (spawnDuration / 0.02))
-        {
-            generateCar();
-            time = 0;
-        }
-    }
+	private void FixedUpdate()
+	{
+		time++;
+		if(time >= (spawnDuration / 0.02))
+		{
+			generateCar();
+			time = 0;
+		}
+	}
 
-    void OnTriggerEnter(Collider other)
-    {
-        Destroy(other.gameObject);
-        currentCars--;
-    }
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy(other.gameObject);
+		currentCars--;
+	}
 
-    private void generateCar()
-    {
-        if (currentCars > maxCars)
-        {
-            return;
-        }
-        GameObject prefab = Resources.Load("jeep", typeof(GameObject)) as GameObject; ;
-        Vector3 vec = new Vector3(22.4f, 1.3f, 1.3f);
-        GameObject prefabInstance = Instantiate(prefab, getChildGameObject(gameObject, "SpawnPoint").transform.position, transform.rotation) as GameObject;
-        if (prefabInstance != null)
-        {
-            var myScriptReference = prefabInstance.GetComponent<FollowWay>();
-            if (myScriptReference != null)
-            {
-                myScriptReference.initialize(startPoint, startDirection);
-            }
-        }
-        currentCars++;
-    }
+	private void generateCar()
+	{
+		if(currentCars > maxCars)
+		{
+			return;
+		}
+		GameObject prefab = Resources.Load("jeep", typeof(GameObject)) as GameObject; ;
+		Vector3 vec = new Vector3(22.4f, 1.3f, 1.3f);
+		GameObject prefabInstance = Instantiate(prefab, getChildGameObject(gameObject, "SpawnPoint").transform.position, transform.rotation) as GameObject;
+		if(prefabInstance != null)
+		{
+			var myScriptReference = prefabInstance.GetComponent<FollowWay>();
+			if(myScriptReference != null)
+			{
+				myScriptReference.initialize(startPoint, startDirection);
+			}
+		}
+		currentCars++;
+	}
 
-    static private GameObject getChildGameObject(GameObject fromGameObject, string withName)
-    {
-        Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
-        foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
-        return null;
-    }
+	static private GameObject getChildGameObject(GameObject fromGameObject, string withName)
+	{
+		Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
+		foreach(Transform t in ts) if(t.gameObject.name == withName) return t.gameObject;
+		return null;
+	}
 
 	// Invoked when the value of the slider changes.
 	public void maxCarsValueChangeCheck()
