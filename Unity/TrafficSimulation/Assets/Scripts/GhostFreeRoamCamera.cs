4f7c53e41518df88f39895b6671bfc3372b74b86
Andreas Reschenhofer <andreas-reschenhofer@gmx.at>
2017-05-06 12:42:31 +0200

    Extended Camera Movement. Added simple terrain.

--- Unity/TrafficSimulation/Assets/Scripts/GhostFreeRoamCamera.cs
+++ Unity/TrafficSimulation/Assets/Scripts/GhostFreeRoamCamera.cs
@@ -13,6 +13,7 @@ public class GhostFreeRoamCamera : MonoBehaviour
     public KeyCode backwardButton = KeyCode.S;
     public KeyCode rightButton = KeyCode.D;
     public KeyCode leftButton = KeyCode.A;
+	public KeyCode shiftLeft = KeyCode.LeftShift;
 
     public float cursorSensitivity = 0.025f;
     public bool cursorToggleAllowed = true;
@@ -35,71 +36,83 @@ public class GhostFreeRoamCamera : MonoBehaviour
 
     private void Update()
     {
-        if (allowMovement)
-        {
-            bool lastMoving = moving;
-            Vector3 deltaPosition = Vector3.zero;
+		if(!Input.GetKey(shiftLeft))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+			GUI.FocusWindow(1);
+			if(allowMovement)
+			{
+				bool lastMoving = moving;
+				Vector3 deltaPosition = Vector3.zero;
 
-            if (moving)
-                currentSpeed += increaseSpeed * Time.deltaTime;
+				if(moving)
+					currentSpeed += increaseSpeed * Time.deltaTime;
 
-            moving = false;
+				moving = false;
 
-            CheckMove(forwardButton, ref deltaPosition, transform.forward);
-            CheckMove(backwardButton, ref deltaPosition, -transform.forward);
-            CheckMove(rightButton, ref deltaPosition, transform.right);
-            CheckMove(leftButton, ref deltaPosition, -transform.right);
+				CheckMove(forwardButton, ref deltaPosition, transform.forward);
+				CheckMove(backwardButton, ref deltaPosition, -transform.forward);
+				CheckMove(rightButton, ref deltaPosition, transform.right);
+				CheckMove(leftButton, ref deltaPosition, -transform.right);
 
-            if (moving)
-            {
-                if (moving != lastMoving)
-                    currentSpeed = initialSpeed;
+				if(moving)
+				{
+					if(moving != lastMoving)
+						currentSpeed = initialSpeed;
 
-                transform.position += deltaPosition * currentSpeed * Time.deltaTime;
-            }
-            else currentSpeed = 0f;            
-        }
+					transform.position += deltaPosition * currentSpeed * Time.deltaTime;
+				}
+				else currentSpeed = 0f;
+			}
 
-        if (allowRotation)
-        {
-            Vector3 eulerAngles = transform.eulerAngles;
-            eulerAngles.x += -Input.GetAxis("Mouse Y") * 359f * cursorSensitivity;
-            eulerAngles.y += Input.GetAxis("Mouse X") * 359f * cursorSensitivity;
-            if (eulerAngles.x < 89 || eulerAngles.x > 271)
-            {
-                transform.eulerAngles = eulerAngles;
-            }
-        }
+			if(allowRotation)
+			{
+				Vector3 eulerAngles = transform.eulerAngles;
+				eulerAngles.x += -Input.GetAxis("Mouse Y") * 359f * cursorSensitivity;
+				eulerAngles.y += Input.GetAxis("Mouse X") * 359f * cursorSensitivity;
+				if(eulerAngles.x < 89 || eulerAngles.x > 271)
+				{
+					transform.eulerAngles = eulerAngles;
+				}
+			}
 
-        if (cursorToggleAllowed)
-        {
-            if (Input.GetKey(cursorToggleButton))
-            {
-                if (!togglePressed)
-                {
-                    togglePressed = true;
-                    if(Cursor.lockState == CursorLockMode.Locked)
-                    {
-                        Cursor.lockState = CursorLockMode.Confined;
-                    }
-                    else
-                    {
-                        Cursor.lockState = CursorLockMode.Locked;
-                    }
-                    //Screen.lockCursor = !Screen.lockCursor;
-                    //Cursor.lockState = !Cursor.lockState;
-                    Cursor.visible = !Cursor.visible;
-                    //Screen.showCursor = !Screen.showCursor;
-                }
-            }
-            else togglePressed = false;
-        }
-        else
-        {
-            togglePressed = false;
-            Cursor.visible = false;
-            //Screen.showCursor = false;
-        }
+			if(cursorToggleAllowed)
+			{
+				if(Input.GetKey(cursorToggleButton))
+				{
+					if(!togglePressed)
+					{
+						togglePressed = true;
+						if(Cursor.lockState == CursorLockMode.Locked)
+						{
+							Cursor.lockState = CursorLockMode.Confined;
+						}
+						else
+						{
+							Cursor.lockState = CursorLockMode.Locked;
+						}
+						//Screen.lockCursor = !Screen.lockCursor;
+						//Cursor.lockState = !Cursor.lockState;
+						Cursor.visible = !Cursor.visible;
+						//Screen.showCursor = !Screen.showCursor;
+					}
+				}
+				else togglePressed = false;
+			}
+			else
+			{
+				togglePressed = false;
+				Cursor.visible = false;
+				//Screen.showCursor = false;
+			}
+		}
+		else
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+        
     }
 
     private void CheckMove(KeyCode keyCode, ref Vector3 deltaPosition, Vector3 directionVector)
